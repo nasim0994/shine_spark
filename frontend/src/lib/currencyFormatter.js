4bce609e44f58db34b9ad289f8764251be4b0b76
nasim0994 <nasimuddin0994@gmail.com>
2025-03-15 16:26:00 +0600

    remobe bdt

--- frontend/src/lib/currencyFormatter.js
+++ frontend/src/lib/currencyFormatter.js
@@ -1,14 +1,13 @@
-const bdRate = 121.39;
+// const bdRate = 121.39;
 
 export const currencyFormatter = (value) => {
-  const countryCode = localStorage.getItem("countryCode");
-
-  let currency = countryCode === "BD" ? "BDT" : "USD";
-  let formattedValue = currency === "BDT" ? value * bdRate : value;
+  // const countryCode = localStorage.getItem("countryCode");
+  // let currency = countryCode === "BD" ? "BDT" : "USD";
+  // let formattedValue = currency === "BDT" ? value * bdRate : value;
 
   return new Intl.NumberFormat("en-US", {
     style: "currency",
-    currency: currency,
+    currency: "USD",
     minimumFractionDigits: 2,
-  }).format(formattedValue);
+  }).format(value);
 };
